49acecf3388b1c10ebdd56db128f5ff615a8ee60
hyphast <oshcherbakov2018@mail.ru>
2021-09-21 23:55:33 +0400

    fix edit appointments

--- src/components/Appointments/AppointmentEdit.jsx
+++ src/components/Appointments/AppointmentEdit.jsx
@@ -16,7 +16,9 @@ import {Typography} from '@material-ui/core';
 import {CustomDateField} from '../fields/CustomDateField/CustomDateField';
 import {format} from 'date-fns';
 import {ru} from 'date-fns/locale'
-
+import { TimeInput } from 'react-admin-date-inputs2';
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 
 const PostEditActions = ({basePath, data, resource}) => (
   <TopToolbar>
@@ -45,22 +47,36 @@ export const AppointmentEdit = (props) => (
     {...props}
   >
     <SimpleForm>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <CustomDateField source="date" label="Дата"/>
       <ArrayInput source="appointments" label='Расписание на этот день'>
         <SimpleFormIterator>
-          <FunctionField label="Время" render={record => format(new Date(record['time']), 'HH:mm', {locale: ru})} />
+          {/*<FunctionField label="Время" render={record => format(new Date(record['time']), 'HH:mm', {locale: ru})} />*/}
+
+            <TimeInput source="time" label="Время"
+                       options=
+                       {{
+                         ampm: false,
+                         okLabel: 'Ок',
+                         cancelLabel: 'Отмена',
+                       }}
+                       providerOptions={{ utils: DateFnsUtils}}
+            />
+
           <NumberInput source="numberPatients" label='Количество пациентов'/>
           <ArrayInput source="patients" label='Записи'>
             <SimpleFormIterator>
               {/*<ReferenceField label="User" source="patientId" reference="users">*/}
               {/*  <TextField source="firstName" />*/}
               {/*</ReferenceField>*/}
-              <TextInput source="patientName" label='Имя' disabled/>
+              {/*<TextInput source="patientId" label='id' disabled/>*/}
+              <TextInput source="patientName" label='Имя'/>
             </SimpleFormIterator>
           </ArrayInput>
+
         </SimpleFormIterator>
       </ArrayInput>
-      <NumberInput source="numberAllPatients" label="Общее количество пациентов" disabled/>
+    </MuiPickersUtilsProvider>
     </SimpleForm>
   </Edit>
 );
